ee7a64100699c9e46d7e5ffc965200ac7ad3e8f9
Shreeya Shrestha <shreeya@moco.com.np>
2025-07-01 15:16:46 +0545

    TEsting actions

---
+++ src/main/java/np/com/moco/githubtest/Main.java
@@ -0,0 +1,14 @@
+package np.com.moco.githubtest;
+
+/**
+ *
+ * @author Shreeya Shrestha <shreeya@moco.com.np>
+ * @Creation Jul 1, 2025 2:06:42 PM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("JAR run. Hello World");
+    }
+
+}
